8887ffb48a83e89426e168769f7dcee4b8dbf726
martinaxgloria <martina.gloria@iit.it>
2023-03-14 12:33:22 +0100

    Trying to display some images when a rpc command is sent

--- src/modules/emotionHandler/emotionHandler.cpp
+++ src/modules/emotionHandler/emotionHandler.cpp
@@ -1,20 +1,33 @@
 #include <yarp/os/ResourceFinder.h>
+#include <yarp/os/LogStream.h>
+#include <yarp/os/Bottle.h>
 #include "emotionHandler.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace yarp::os;
+
+EmotionHandler::EmotionHandler()
+{
+}
+
+EmotionHandler::~EmotionHandler()
+{
+}
 
 bool EmotionHandler::configure(ResourceFinder& config)
 {
+    rpcPort.open("/emotionHandler/rpc");
+    attach(rpcPort);
 
     return true;
 }
 
 bool EmotionHandler::close()
 {
-
-    return true;
-}
-
-bool EmotionHandler::interruptModule()
-{
+    rpcPort.close();
 
     return true;
 }
@@ -32,40 +45,85 @@ bool EmotionHandler::updateModule()
 
 bool EmotionHandler::respond(const Bottle &cmd, Bottle &reply)
 {
-    switch(command.get(0).asVocab32())
+    if(cmd.size() < 2)
+    {
+        yError() << "Invalid command size: it must be 2";
+        return false;
+    }
+
+    switch (cmd.get(0).asVocab32())
     {
         case EMOTION_VOCAB_SET:
         {
-            switch (command.get(1).asVocab32())
+            namedWindow("emotion", WINDOW_NORMAL);
+            bool ok = false;
+            reply.clear();
+
+            switch (cmd.get(1).asVocab32())
             {
-            case EMOTION_VOCAB_ANGRY:
-                /* code */
-                break;
-            case EMOTION_VOCAB_CUN:
-                /* code */
-                break;
-            case EMOTION_VOCAB_EVIL:
-                /* code */
-                break;
-            case EMOTION_VOCAB_HAPPY:
-                /* code */
-                break;
-            case EMOTION_VOCAB_NEUTRAL:
-                /* code */
-                break;
-            case EMOTION_VOCAB_SAD:
-                /* code */
-                break;
-            case EMOTION_VOCAB_SHY:
-                /* code */
-                break;
-            case EMOTION_VOCAB_SURPRISED:
-                /* code */
-                break;
-            default:
-                break;
+                case EMOTION_VOCAB_ANGRY:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_CUN:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_EVIL:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_HAPPY:
+                {                  
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_NEUTRAL:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_SAD:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_SHY:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                case EMOTION_VOCAB_SURPRISED:
+                {
+                    ok = getCommand(cmd.get(1).toString());
+                    break;
+                }
+                default:
+                {
+                    yDebug() << "Command not recognized!";
+                    break;
+                }
             }
+            reply.addVocab32(EMOTION_VOCAB_OK);
         }
     }
+    return true;
+}
+
+bool EmotionHandler::getCommand(std::string command)
+{
+    Mat image = imread("/home/martinagloria/ergocub-software/src/modules/emotionHandler/images/" + command + ".png");
+
+    if(image.empty())
+    {
+        yDebug() << "Could not read the image!";
+    }
+    
+    imshow("emotion", image);
+    waitKey(0);
+
     return true;
 }
---
+++ src/modules/emotionHandler/main.cpp
@@ -0,0 +1,23 @@
+#include <yarp/os/Network.h>
+#include <yarp/os/LogStream.h>
+#include "emotionHandler.h"
+
+using namespace yarp::os;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+    if(!yarp.checkNetwork())
+    {
+        yError()<<"Yarp doesn't work!";
+        return EXIT_FAILURE;
+    }
+    
+    ResourceFinder rf;
+    rf.configure(argc, argv);
+    EmotionHandler emotionHandler;
+    emotionHandler.configure(rf);
+
+    return emotionHandler.runModule();
+}
