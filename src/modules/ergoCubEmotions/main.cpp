c13688c8288be3945a4503387bbad0ae67ca20c3
martinaxgloria <martina.gloria@iit.it>
2023-03-28 12:38:31 +0200

    Fixing the yarp-config import

--- src/modules/ergoCubEmotions/main.cpp
+++ src/modules/ergoCubEmotions/main.cpp
@@ -27,7 +27,5 @@ int main(int argc, char *argv[])
     rf.configure(argc, argv);
 
     ErgoCubEmotions ergoCubEmotions;
-    ergoCubEmotions.configure(rf);
-
-    return ergoCubEmotions.runModule();
+    return ergoCubEmotions.runModule(rf);
 }
