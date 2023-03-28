5b389ff89931609c7e7fdc3820334bdcd5e6751b
martinaxgloria <martina.gloria@iit.it>
2023-03-28 10:31:06 +0200

    Add the documentation

--- src/modules/ergoCubEmotions/idl.thrift
+++ src/modules/ergoCubEmotions/idl.thrift
@@ -6,7 +6,22 @@
  *                                                                            *
  ******************************************************************************/
 
+/**
+ * @file idl.thrift
+ */
+
+/**
+ * ergoCubEmotions_IDL
+ *
+ * IDL Interface to ergoCub Emotion Handler services.
+ */
+
 service ergoCubEmotions_IDL
 {
+        /**
+        * Display a specific facial expression.
+        * @param command is the name of the emotion that it would be set.
+        * @return true/false on success/failure.
+        */
         bool setEmotion(1:string command);
 }
