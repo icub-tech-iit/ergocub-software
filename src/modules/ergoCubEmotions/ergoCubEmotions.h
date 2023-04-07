89bfe9a052b20b388503b331de485fd207664a80
martinaxgloria <martina.gloria@iit.it>
2023-04-07 11:16:17 +0200

    ergoCubEmotions refactoring in order to play video

--- src/modules/ergoCubEmotions/ergoCubEmotions.h
+++ src/modules/ergoCubEmotions/ergoCubEmotions.h
@@ -22,22 +22,6 @@
 
 #include "ergoCubEmotions_IDL.h"
 
-// define the vocabs
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_SET = yarp::os::createVocab32('s','e','t');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_GET = yarp::os::createVocab32('g','e','t');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_IS  = yarp::os::createVocab32('i','s');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_FAILED = yarp::os::createVocab32('f','a','i','l');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_OK = yarp::os::createVocab32('o','k');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_HELP = yarp::os::createVocab32('h','e','l','p');
-
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_NEUTRAL = yarp::os::createVocab32('n','e','u');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_HAPPY = yarp::os::createVocab32('h','a','p');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_SAD = yarp::os::createVocab32('s','a','d');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_SURPRISED = yarp::os::createVocab32('s','u','r');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_ANGRY = yarp::os::createVocab32('a','n','g');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_EVIL = yarp::os::createVocab32('e','v','i');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_SHY = yarp::os::createVocab32('s','h','y');
-// constexpr  yarp::conf::vocab32_t EMOTION_VOCAB_CUN = yarp::os::createVocab32('c','u','n');
 
 class ErgoCubEmotions : public yarp::os::RFModule, public ergoCubEmotions_IDL {
     protected:
@@ -57,11 +41,10 @@ class ErgoCubEmotions : public yarp::os::RFModule, public ergoCubEmotions_IDL {
         std::vector<std::string> availableEmotions();
 
         yarp::os::RpcServer cmdPort;
-
         int nexpressions;
         std::string path;
         std::map<std::string, std::pair<std::string, std::string>> img_map;
+        std::string command;
         cv::Mat img;
 };
-
 #endif
