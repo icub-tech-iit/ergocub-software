f82590b57a0bb9cb72b8b5579969d8a16e8b9b07
martinaxgloria <martina.gloria@iit.it>
2023-04-20 11:04:12 +0200

    Add to ergoCubEmotions boundary conditions

--- src/modules/ergoCubEmotions/ergoCubEmotions.cpp
+++ src/modules/ergoCubEmotions/ergoCubEmotions.cpp
@@ -32,11 +32,17 @@ bool ErgoCubEmotions::configure(ResourceFinder& rf)
     Bottle &bGroup = rf.findGroup("general");
     nExpressions = bGroup.find("num_expressions").asInt32();
     nTransitions = bGroup.find("num_transitions").asInt32();
-    for (int i = 0; i < nExpressions; i++)
+
+    for(int i = 0; i < nExpressions; i++)
     {
         std::ostringstream expression_i;
         expression_i << "expression_" << i;
         Bottle &bExpression = rf.findGroup(expression_i.str());
+        if(bExpression.get(0).asString().empty())
+        {
+            yError() << "Missing expression" << i << "in config file!";
+            return false;
+        }
         std::string name = bExpression.find("name").asString();
         std::string type = bExpression.find("type").asString();
         std::string file = bExpression.find("file").asString();
@@ -47,17 +53,39 @@ bool ErgoCubEmotions::configure(ResourceFinder& rf)
         imgMap[name] = par;
     }
 
-    for (int j = 0; j < nTransitions; j++)
+    // if(imgMap.size() < nExpressions)
+    // {
+    //     yError() << "The available expressions are lower than the actual number of inputs!";
+    //     return false;
+    // }
+
+    for(int j = 0; j < nTransitions; j++)
     {
         std::ostringstream transition_j;
         transition_j << "transition_" << j;
         Bottle &bTransition = rf.findGroup(transition_j.str());
+        if(bTransition.get(0).asString().empty())
+        {
+            yError() << "Missing transition" << j << "in config file!";
+            return false;
+        }
         std::string source = bTransition.find("source").asString();
         std::string destination = bTransition.find("destination").asString();
+
+        if(imgMap.count(source) < 1 || imgMap.count(destination) < 1)
+        {
+            yError() << "Transition" << j << "is pointing to a non existing emotion!";
+            return false;
+        }
         std::string file = bTransition.find("file").asString();
         
         std::pair<std::string, std::string> par = std::make_pair(source, destination);
         transitionMap[par] = file;
+    }
+
+    if(transitionMap.size() > nTransitions)
+    {
+        yError() << "The available transitions are higher than the actual number of inputs!";
     }
 
     isTransition = true;
@@ -100,6 +128,10 @@ bool ErgoCubEmotions::updateModule()
         {   
             if(it->second.first == "image")
             {   
+                if(isTransition)
+                {
+                    showTransition();
+                }
                 path = rf->findFile(it->second.second);
                 Mat img_tmp = imread(path);
                 if(img_tmp.empty())
@@ -144,7 +176,7 @@ bool ErgoCubEmotions::setEmotion(const std::string& command)
 {   
     if(imgMap.count(command) < 1)
     {
-        yError() << "Command not found!";
+        yError() << command << "not found!";
         return false;
     }
 
