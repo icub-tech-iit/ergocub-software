e8602a6b067a8245580c67eb8969d64bf50b2f7c
Mattia Fussi <mattia.fussi@iit.it>
2024-12-12 13:52:37 +0100

    create separate thread to gather data and improve parameters

--- src/tools/ergocub-fingers-tuning/cpp/ident-finger/main.cpp
+++ src/tools/ergocub-fingers-tuning/cpp/ident-finger/main.cpp
@@ -20,9 +20,9 @@
 #include <yarp/os/Bottle.h>
 #include <yarp/os/LogStream.h>
 #include <yarp/os/Network.h>
+#include <yarp/os/PeriodicThread.h>
 #include <yarp/os/ResourceFinder.h>
 #include <yarp/os/Time.h>
-#include <yarp/os/Value.h>
 
 using namespace yarp::os;
 using namespace yarp::dev;
@@ -35,8 +35,91 @@ struct DataExperiment {
   double pid_out;
 };
 
+IPositionControl *iPos{nullptr};
+IPWMControl *iPwm{nullptr};
+IControlMode *iCm{nullptr};
+IEncoders *iEnc{nullptr};
+IPidControl *iPid{nullptr};
+
+double t0 = 0;
+
+std::vector<DataExperiment> data_vec;
+
+class PWMThread : public PeriodicThread {
+private:
+  ResourceFinder rf_;
+  bool done_h = false;
+  bool done_l = false;
+  DataExperiment data;
+  uint8_t j = 0;
+  double pwm_ = 0;
+  double thr_min = 0;
+  double thr_max = 0;
+  double v = 0;
+
+public:
+  PWMThread(double ts, ResourceFinder &rf, uint8_t joint_id)
+      : PeriodicThread(ts), rf_(rf), j(joint_id) {}
+
+  bool threadInit() {
+    auto limits = rf_.find("limits").asList();
+    thr_min = limits->get(0).asFloat64();
+    thr_max = limits->get(1).asFloat64();
+    return true;
+  }
+
+  void run() {
+    data.t = Time::now() - t0;
+    data.pwm_set = pwm_;
+    iEnc->getEncoder(j, &data.enc);
+    iPid->getPidOutput(VOCAB_PIDTYPE_POSITION, j, &data.pid_out);
+    iPwm->getDutyCycle(j, &data.pwm_read);
+
+    if (data.enc >= thr_max && pwm_ > .1) {
+      done_h = true;
+      pwm_ = -pwm_;
+      iPwm->setRefDutyCycle(j, pwm_);
+
+    } else if (data.enc <= thr_min && pwm_ < -0.1) {
+      done_l = true;
+      pwm_ = -pwm_;
+      iPwm->setRefDutyCycle(j, pwm_);
+    }
+    data_vec.push_back(std::move(data));
+  }
+
+  bool isDone() { return done_h && done_l; }
+
+  void setInputs(double pwm) {
+    pwm_ = pwm;
+    iPwm->setRefDutyCycle(j, pwm_);
+  }
+
+  void resetFlags() {
+    done_h = false;
+    done_l = false;
+  }
+};
+
+bool resetPosition(uint8_t j) {
+  iPwm->setRefDutyCycle(j, 0.0);
+  iCm->setControlMode(j, VOCAB_CM_POSITION);
+  iPos->setRefSpeed(j, 25.0);
+  iPos->setRefAcceleration(j, std::numeric_limits<double>::max());
+  iPos->positionMove(j, 0.);
+  bool done = false;
+
+  while (!done) {
+    iPos->checkMotionDone(j, &done);
+    Time::yield();
+  }
+
+  return done;
+}
+
 int main(int argc, char *argv[]) {
   Network yarp;
+
   if (!yarp.checkNetwork()) {
     yError() << "Unable to find YARP server!";
     return EXIT_FAILURE;
@@ -44,29 +127,24 @@ int main(int argc, char *argv[]) {
 
   ResourceFinder rf;
   rf.configure(argc, argv);
+  rf.setQuiet(true);
 
   auto port = rf.check("port", Value("/ergocub/left_arm")).asString();
-  auto joint_id = rf.check("joint-id", Value(1)).asInt8();
   auto cycles = rf.check("cycles", Value(10)).asInt32();
-  auto threshold = rf.check("threshold", Value(20)).asInt32();
-  auto timeout = rf.check("timeout", Value(5000)).asInt32();
+  auto timeout = rf.check("timeout", Value(5.0)).asFloat64();
   auto filename = rf.check("filename", Value("output.csv")).asString();
-  auto pwm_list = rf.find("pwm_values").asList();
-
-  PolyDriver m_driver;
-  IPositionControl *iPos{nullptr};
-  IPWMControl *iPwm{nullptr};
-  IControlMode *iCm{nullptr};
-  IEncoders *iEnc{nullptr};
-  IPidControl *iPid{nullptr};
+  auto pwm_list = rf.find("pwm-values").asList();
+  auto Ts = rf.check("period", Value(0.001)).asFloat64();
+  auto joint_id = rf.check("joint-id", Value(12)).asInt8();
 
   Property conf;
   conf.put("device", "remote_controlboard");
   conf.put("remote", port);
   conf.put("local", "/logger");
 
+  PolyDriver m_driver;
   if (!m_driver.open(conf)) {
-    yError() << "Failed to open";
+    yError() << "Failed to open" << port;
     return EXIT_FAILURE;
   }
 
@@ -77,93 +155,45 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  std::vector<double> pwm_values; //{10, 25, 50, 75};
+  std::vector<double> pwm_values;
 
   for (size_t i = 0; i < pwm_list->size(); ++i) {
     pwm_values.push_back(pwm_list->get(i).asFloat64());
   }
 
+  std::unique_ptr<PWMThread> pwm_thr =
+      std::make_unique<PWMThread>(Ts, rf, joint_id);
+
   std::ofstream file;
-
   file.open(filename);
-
-  std::vector<DataExperiment> data_vec;
   data_vec.reserve(100000);
 
-  // set control mode to position
-  iCm->setControlMode(joint_id, VOCAB_CM_POSITION);
-  // Homing
-  iPos->setRefSpeed(joint_id, 5.);
-  iPos->setRefAcceleration(joint_id, std::numeric_limits<double>::max());
-  iPos->positionMove(joint_id, 0.);
-  auto done{false};
-  while (!done) {
-    iPos->checkMotionDone(joint_id, &done);
-    Time::yield();
-  }
+  resetPosition(joint_id);
 
-  DataExperiment data;
-  auto t0 = Time::now();
-  auto t1{t0};
-
-  // set control mode to pwm
   iCm->setControlMode(joint_id, VOCAB_CM_PWM);
   iPwm->setRefDutyCycle(joint_id, 0.0);
 
-  // drives the joint between +20/-20 degrees for each vlaue of PWM stored in
-  // pwm_values
+  t0 = Time::now();
+  pwm_thr->start();
+
   for (auto pwm : pwm_values) {
-    uint t = 0;
-    for (int i = 0; i < cycles * 2; i++) {
-      auto done_h = false;
-      auto done_l = false;
-      done = false;
-      pwm = -pwm;
-      iPwm->setRefDutyCycle(joint_id, pwm);
-
-      while ((!done_h || !done_l) && t < timeout) {
-        data.t = Time::now() - t0;
-        data.pwm_set = pwm;
-        iEnc->getEncoder(joint_id, &data.enc);
-        iPid->getPidOutput(VOCAB_PIDTYPE_POSITION, joint_id, &data.pid_out);
-        iPwm->getDutyCycle(joint_id, &data.pwm_read);
-
-        // if the joint exceeds +20/-20 degrees invert the PWM
-        if (Time::now() - t1 >= .001) {
-          //       yDebug() << data.pwm << " " << data.enc;
-          t++;
-          if (data.enc > threshold) {
-            if (!done_h) {
-              pwm = -pwm;
-              iPwm->setRefDutyCycle(joint_id, pwm);
-              done_h = true;
-            }
-          } else if (data.enc < threshold * -1) {
-            if (!done_l) {
-              pwm = -pwm;
-              iPwm->setRefDutyCycle(joint_id, pwm);
-              done_l = true;
-            }
-          }
-          data_vec.push_back(std::move(data));
-          t1 = Time::now();
+    for (int i = 0; i < cycles; i++) {
+      pwm_thr->setInputs(pwm);
+      yDebug() << "(pwm, cycle n): (" << pwm << i << ")";
+      while (true) {
+        if (pwm_thr->isDone() || pwm_thr->getIterations() > (timeout / Ts)) {
+          pwm_thr->resetFlags();
+          pwm_thr->resetStat();
+          break;
         }
       }
     }
   }
-  // sets PWM to zero
-  iPwm->setRefDutyCycle(joint_id, 0.0);
-  // sets control mode to position
-  iCm->setControlMode(joint_id, VOCAB_CM_POSITION);
-  // Homing
-  iPos->setRefSpeed(joint_id, 5.);
-  iPos->setRefAcceleration(joint_id, std::numeric_limits<double>::max());
-  iPos->positionMove(joint_id, 0.);
-  while (!done) {
-    iPos->checkMotionDone(joint_id, &done);
-    Time::yield();
-  }
-  // writes the log
+
+  pwm_thr->stop();
+
+  resetPosition(joint_id);
+
   for (const auto &d : data_vec) {
     file << d.t << "," << d.pwm_read << "," << d.enc << "," << d.pid_out << ","
          << d.pwm_set << std::endl;
@@ -172,6 +202,6 @@ int main(int argc, char *argv[]) {
   m_driver.close();
   file.close();
 
-  yInfo() << "Main returning...";
+  yInfo() << "Done.";
   return EXIT_SUCCESS;
 }
